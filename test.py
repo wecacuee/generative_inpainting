2c0ae1dc7eaa496309b6086b9c963a92414bac18
Vikas Dhiman <wecacuee@users.noreply.github.com>
2019-11-14 14:42:30 -0800

    Made a class for the prediction model

--- test.py
+++ test.py
@@ -18,9 +18,49 @@ parser.add_argument('--output', default='output.png', type=str,
 parser.add_argument('--checkpoint_dir', default='', type=str,
                     help='The directory of tensorflow checkpoint.')
 
+class FillInpainting:
+    def __init__(self, checkpoint_dir, config='inpaint.yml'):
+        self.FLAGS = ng.Config(config)
+        self.checkpoint_dir = checkpoint_dir
+
+    def predict(self, image, mask):
+        FLAGS = self.FLAGS
+        assert image.shape == mask.shape
+
+        h, w, _ = image.shape
+        grid = 8
+        image = image[:h//grid*grid, :w//grid*grid, :]
+        mask = mask[:h//grid*grid, :w//grid*grid, :]
+        print('Shape of image: {}'.format(image.shape))
+
+        image = np.expand_dims(image, 0)
+        mask = np.expand_dims(mask, 0)
+        input_image = np.concatenate([image, mask], axis=2)
+
+        sess_config = tf.ConfigProto()
+        sess_config.gpu_options.allow_growth = True
+        with tf.Session(config=sess_config) as sess:
+            input_image = tf.constant(input_image, dtype=tf.float32)
+            output = model.build_server_graph(FLAGS, input_image)
+            output = (output + 1.) * 127.5
+            output = tf.reverse(output, [-1])
+            output = tf.saturate_cast(output, tf.uint8)
+            # load pretrained model
+            vars_list = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES)
+            assign_ops = []
+            for var in vars_list:
+                vname = var.name
+                from_name = vname
+                var_value = tf.contrib.framework.load_variable(self.checkpoint_dir, from_name)
+                assign_ops.append(tf.assign(var, var_value))
+            sess.run(assign_ops)
+            print('Model loaded.')
+            result = sess.run(output)
+            return result[0]
+
+
 
 if __name__ == "__main__":
-    FLAGS = ng.Config('inpaint.yml')
     # ng.get_gpus(1)
     args, unknown = parser.parse_known_args()
 
@@ -33,35 +73,6 @@ if __name__ == "__main__":
         raise ValueError('File not found: {}'.format(args.mask))
     # mask = cv2.resize(mask, (0,0), fx=0.5, fy=0.5)
 
-    assert image.shape == mask.shape
-
-    h, w, _ = image.shape
-    grid = 8
-    image = image[:h//grid*grid, :w//grid*grid, :]
-    mask = mask[:h//grid*grid, :w//grid*grid, :]
-    print('Shape of image: {}'.format(image.shape))
-
-    image = np.expand_dims(image, 0)
-    mask = np.expand_dims(mask, 0)
-    input_image = np.concatenate([image, mask], axis=2)
-
-    sess_config = tf.ConfigProto()
-    sess_config.gpu_options.allow_growth = True
-    with tf.Session(config=sess_config) as sess:
-        input_image = tf.constant(input_image, dtype=tf.float32)
-        output = model.build_server_graph(FLAGS, input_image)
-        output = (output + 1.) * 127.5
-        output = tf.reverse(output, [-1])
-        output = tf.saturate_cast(output, tf.uint8)
-        # load pretrained model
-        vars_list = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES)
-        assign_ops = []
-        for var in vars_list:
-            vname = var.name
-            from_name = vname
-            var_value = tf.contrib.framework.load_variable(args.checkpoint_dir, from_name)
-            assign_ops.append(tf.assign(var, var_value))
-        sess.run(assign_ops)
-        print('Model loaded.')
-        result = sess.run(output)
-        cv2.imwrite(args.output, result[0][:, :, ::-1])
+    fill_inp = FillInpainting(args.checkpoint_dir)
+    result = fill_inp.predict(image, mask)
+    cv2.imwrite(args.output, result[:, :, ::-1])

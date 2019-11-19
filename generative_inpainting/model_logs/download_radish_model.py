fcacdcb5c11872c6ef7d3a9e4fd033d66e3a91c8
Vikas Dhiman <wecacuee@users.noreply.github.com>
2019-11-18 23:44:57 -0800

    great

--- generative_inpainting/model_logs/download_radish_model.py
+++ generative_inpainting/model_logs/download_radish_model.py
@@ -6,12 +6,9 @@ def download(target_dir):
     dirtomake = osp.dirname(target_dir)
     if not osp.exists(dirtomake):
         os.makedirs(dirtomake)
-    p1 = subprocess.Popen(
-        """wget https://www.dropbox.com/s/lz19pn97xc5f0ko/generative-inpainting-radish-model.tgz?dl=0 -O -""".split(), stdout=subprocess.PIPE)
-    p2 = subprocess.run("""tar -C {target_dir} xvf -""".format(target_dir=target_dir).split(),
-                   stdin=p1.PIPE)
-    p1.stdout.close()
-    p2.communicate()
+    subprocess.check_call(
+        """wget https://www.dropbox.com/s/lz19pn97xc5f0ko/generative-inpainting-radish-model.tgz?dl=0 -O - | tar -C {target_dir} -xvf -""".format(target_dir=target_dir), shell=True)
 
 if __name__ == '__main__':
-    download()
+    import sys
+    download(sys.argv[1])

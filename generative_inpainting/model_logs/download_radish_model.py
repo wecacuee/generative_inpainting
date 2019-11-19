68b7ca7af43c6085eb32eed94161be83109bf379
Vikas Dhiman <wecacuee@users.noreply.github.com>
2019-11-18 23:34:29 -0800

    python makes bash code complicated

--- generative_inpainting/model_logs/download_radish_model.py
+++ generative_inpainting/model_logs/download_radish_model.py
@@ -4,8 +4,10 @@ import subprocess
 
 def download(target_dir):
     os.makedirs(osp.dirname(target_dir))
-    subprocess.run(
-        """wget https://www.dropbox.com/s/lz19pn97xc5f0ko/generative-inpainting-radish-model.tgz?dl=0 -O - | tar -C {target_dir} xvf -""".format(target_dir=target_dir).split())
+    p1 = subprocess.Popen(
+        """wget https://www.dropbox.com/s/lz19pn97xc5f0ko/generative-inpainting-radish-model.tgz?dl=0 -O -""".split(), stdout=subprocess.PIPE)
+    subprocess.run("""tar -C {target_dir} xvf -""".format(target_dir=target_dir).split(),
+                   stdin=p1.PIPE)
 
 if __name__ == '__main__':
     download()

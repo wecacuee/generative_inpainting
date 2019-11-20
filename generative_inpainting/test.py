8309187db963db465e3544f4974cd6918e6b052c
Vikas Dhiman <wecacuee@users.noreply.github.com>
2019-11-19 19:09:44 -0800

    works for batch tests

--- generative_inpainting/test.py
+++ generative_inpainting/test.py
@@ -1,5 +1,6 @@
 import os.path as osp
 import subprocess
+import sys
 
 import cv2
 import numpy as np
@@ -28,46 +29,66 @@ class FillInpainting:
     def __init__(self,
                  checkpoint_dir=None,
                  get_checkpoint_dir=download_radish,
-                 config=abspath('inpaint.yml')):
+                 config=abspath('inpaint.yml'),
+                 max_size=(1024, 1024)):
         self.FLAGS = ng.Config(config)
+        self.max_size = max_size
         if checkpoint_dir is None:
             checkpoint_dir = get_checkpoint_dir()
         self.checkpoint_dir = checkpoint_dir
+        self.model_loaded = False
+
+    def load_model(self, image_shape):
+        sess_config = tf.ConfigProto()
+        sess_config.gpu_options.allow_growth = True
+        self.session = sess = tf.Session(config=sess_config)
+
+        self.model = model = InpaintCAModel()
+        self.input_image_ph = tf.placeholder(
+            tf.float32, shape=(1, image_shape[0], image_shape[1]*2, 3))
+        output = model.build_server_graph(self.FLAGS, self.input_image_ph)
+        output = (output + 1.) * 127.5
+        output = tf.reverse(output, [-1])
+        self.output = output = tf.saturate_cast(output, tf.uint8)
+        vars_list = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES)
+        assign_ops = []
+        for var in vars_list:
+            vname = var.name
+            from_name = vname
+            var_value = tf.contrib.framework.load_variable(
+                self.checkpoint_dir, from_name)
+            assign_ops.append(tf.assign(var, var_value))
+        sess.run(assign_ops)
+        self.model_loaded = True
+        print('Model loaded.')
+
+
 
     def predict(self, image, mask):
         FLAGS = self.FLAGS
+        max_size= self.max_size
         assert image.shape == mask.shape
-        model = InpaintCAModel()
 
         h, w, _ = image.shape
+        print('Raw Shape of image: {}'.format(image.shape), file=sys.stderr, flush=True)
+        old_size = image.shape[:2]
+        if np.prod(image.shape) > np.prod(max_size)*3:
+            image = cv2.resize(image, max_size)
+            mask = cv2.resize(mask, max_size)
         grid = 8
         image = image[:h//grid*grid, :w//grid*grid, :]
         mask = mask[:h//grid*grid, :w//grid*grid, :]
-        print('Shape of image: {}'.format(image.shape))
+        print('Grid Shape of image: {}'.format(image.shape), file=sys.stderr, flush=True)
+        if not self.model_loaded:
+            self.load_model(image.shape[:2])
 
         image = np.expand_dims(image, 0)
         mask = np.expand_dims(mask, 0)
         input_image = np.concatenate([image, mask], axis=2)
 
-        sess_config = tf.ConfigProto()
-        sess_config.gpu_options.allow_growth = True
-        with tf.Session(config=sess_config) as sess:
-            input_image = tf.constant(input_image, dtype=tf.float32)
-            output = model.build_server_graph(FLAGS, input_image)
-            output = (output + 1.) * 127.5
-            output = tf.reverse(output, [-1])
-            output = tf.saturate_cast(output, tf.uint8)
-            # load pretrained model
-            vars_list = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES)
-            assign_ops = []
-            for var in vars_list:
-                vname = var.name
-                from_name = vname
-                var_value = tf.contrib.framework.load_variable(self.checkpoint_dir, from_name)
-                assign_ops.append(tf.assign(var, var_value))
-            sess.run(assign_ops)
-            print('Model loaded.')
-            result = sess.run(output)
-            return result[0]
+        # load pretrained model
+        result = self.session.run(self.output, feed_dict={self.input_image_ph: input_image})
+        return cv2.resize(result[0], old_size)
 
+GLOBAL_FILL_INPAINTING = FillInpainting()
 
